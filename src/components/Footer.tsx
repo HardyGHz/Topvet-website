f2fba47375b6e74070008217a31f3afcd1e18db2
HardyGHz <richardkiraly.13@gmail.com>
2026-03-24 14:41:59 +0200

    fix: remove unused imports fixing deployment build errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { MapPin, Phone, Clock, Mail } from 'lucide-react';
 
 export default function Footer() {
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { motion } from 'framer-motion';
 import { Stethoscope, Syringe, HeartPulse, ShoppingBag, Scissors, CalendarCheck } from 'lucide-react';
 
